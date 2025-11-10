c287250a0cc6e48a431a4b41f8337daf7b9dce14
josh <anthony21josh@gmail.com>
2025-11-10 21:03:20 +0800

    removes basic template of vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const fetchData = async () => {
-    const response = await fetch("http://127.0.0.1:3050/")
-    const data = await response.json()
-    console.log(data)
-  }
-	
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Testing Hot Reload</h1>
-      <div className="card">
-        <button onClick={fetchData}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const fetchData = async () => {
+		const response = await fetch("http://127.0.0.1:3050/")
+		const data = await response.json()
+		console.log(data)
+	}
+
+	return (
+		<>
+			<div>
+				<button onSubmit={fetchData}>Testing Backend!</button>
+			</div>
+		</>
+	)
 }
 
 export default App
